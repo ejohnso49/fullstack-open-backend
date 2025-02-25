ac4e5f085bb1f482e1e11ddd55f87b4dd7388aa5
Eric Johnson <eric.j.johnson22@gmail.com>
2025-02-24 19:10:57 -0600

    Complete exercise 3.2

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get('/api/persons', (request, response) => {
   response.json(phonebook);
 });
 
+app.get('/info', (request, response) => {
+  const date = new Date().toUTCString();
+  const length = phonebook.length;
+
+  const payload = `<p>Phonebook has info for ${length} people</p>` + `<p>${date}</p>`;
+
+  response.send(payload);
+  response.end();
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
